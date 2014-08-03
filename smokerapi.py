15cc78e52a749d838dff7bdb33f5996af21a91cb
Andrew Zenk <andrew@andrewzenk.com>
2014-08-03 10:56:05 -0500

    Removed socketio test code

--- smokerapi.py
+++ smokerapi.py
@@ -6,15 +6,11 @@ from flask import request
 
 from datetime import datetime
 
-from flask.ext.socketio import SocketIO, emit
-
 from smokerlib import *
 
 import psycopg2
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 app.debug = True
 
 def get_db():
@@ -115,27 +111,6 @@ def get_battery_charge(smoker_id):
 		charge = 0
 
 	return jsonify(time=val[0],value=charge,units='%')
-	
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']})
-
-@socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0",port=8000)
